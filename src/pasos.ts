194f6291fc244d2f8f30b484cd156c45ecdff982
SilvioAlvarez67 <104239950+SilvioAlvarez67@users.noreply.github.com>
2022-04-26 22:14:25 -0300

    Initial commit

---
+++ src/pasos.ts
@@ -0,0 +1,12 @@
+console.log ("pasos para hacer una torta");
+console.log ("paso 1");
+console.log ("ingredientes");
+console.log ("harina, huevos, sal, manteca, vainilla");
+console.log ("paso 2");
+console.log ("unir la harina, los huevos, la manteca, la sal y la vainilla");
+console.log ("paso 3");
+console.log ("vertir la mezcla en una tortera");
+console.log ("paso 4");
+console.log ("llevar la tortera al horno por 40 minutos");
+console.log ("paso 5");
+console.log ("retirar la torta y disfrutar";
